e7b42beb0a048c05f2dd902a4a097439be56b2fb
Bozhdidar <bozidar.hristov@gmail.com>
2017-11-16 18:31:29 +0100

    DJs finally aligned centrally

--- src/components/dj-card.js
+++ src/components/dj-card.js
@@ -6,20 +6,21 @@ import sc from '../img/soundcloud.svg'
 
 export default ({ data }) => {
   return(
-    <section className="tc pa3 pa5-ns fl w-33">
-      <article className="hide-child relative ba b--black-20 mw5 center">
+    // tc pa3 pa4-ns fl w-l
+    <section className="fl tc w-50 w-third-m w-25-l pa2">
+      <article className="center hide-child relative ba b--black-20 mw5 db">
         <img src={mfdoom} className="db" alt="DJ Photo" />
         <div className="bg-white pa2 bt b--black-20">
           <a className="black-70 ttu tracked tc fw4 f4 db link black hover-black-60" href="#">MF Doom</a>
           {/* <p className="f6 gray mv1">Social Media</p> */}
           <div className="pv2">
-            <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" href="https://www.facebook.com/mfdoom/" title="DJ FB">
+            <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" target="_blank" href="https://www.facebook.com/mfdoom/" title="DJ FB">
               <img className="h1" src={fb}></img>
             </a>
-            <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" href="https://twitter.com/mfdoom?lang=en" title="DJ Twitter">
+            <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" target="_blank" href="https://twitter.com/mfdoom?lang=en" title="DJ Twitter">
               <img className="h1" src={twitter}></img>
             </a>
-            <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" href="https://soundcloud.com/mf-doom" title="DJ Soundcloud">
+            <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" target="_blank" href="https://soundcloud.com/mf-doom" title="DJ Soundcloud">
               <img className="h1" src={sc}></img>
             </a>
           </div>
--- src/components/djs.js
+++ src/components/djs.js
@@ -4,16 +4,14 @@ import DjCard from './dj-card';
 export default ({ data }) => {
   return(
     <article className="w-100 bg-light-gray pv2" >
-      <div className="cf pa2">
-        <h2 className="center black-70 ttu tracked f1 tc fw4">DJs</h2>
-        <div className="center" >
+      <h2 className="center black-70 ttu tracked f1 tc fw4">DJs</h2>
+      <div className="cf pa2" >
         <DjCard />
         <DjCard />
         <DjCard />
         <DjCard />
         <DjCard />
         <DjCard />
-      </div>
       </div>
     </article>
   );
