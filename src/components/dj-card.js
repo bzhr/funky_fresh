8d2cd5fd8564f714257dcf74c912cada52323f25
Bozhdidar <bozidar.hristov@gmail.com>
2017-11-16 04:13:27 +0100

    Social media buttons

--- src/components/dj-card.js
+++ src/components/dj-card.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import mfdoom from '../pages/djs/img/mfdoom.jpg'
+import mfdoom from '../pages/djs/img/mfdoom.jpg';
+import fb from '../img/facebook.svg';
+import twitter from '../img/twitter.svg';
+import sc from '../img/soundcloud.svg'
 
 export default ({ data }) => {
   return(
@@ -9,6 +12,17 @@ export default ({ data }) => {
         <div className="pa2 bt b--black-20">
           <a className="f6 db link black hover-black-60" href="#">MF Doom</a>
           <p className="f6 gray mv1">Social Media</p>
+          <div className="pv1">
+            <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" href="https://www.facebook.com/mfdoom/" title="DJ FB">
+              <img className="h1" src={fb}></img>
+            </a>
+            <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" href="https://twitter.com/mfdoom?lang=en" title="DJ Twitter">
+              <img className="h1" src={twitter}></img>
+            </a>
+            <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" href="https://soundcloud.com/mf-doom" title="DJ Soundcloud">
+              <img className="h1" src={sc}></img>
+            </a>
+          </div>
           <a className="link tc ph3 pv1 db bg-animate bg-near-black hover-bg-black-60 white f6 br1" href="#">Download promo pack</a>
         </div>
       </article>
