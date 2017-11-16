56f637edb576bd0ef7f649af9dec9f1704ff1005
Bozhdidar <bozidar.hristov@gmail.com>
2017-11-16 03:42:41 +0100

    Footer da ne jebe

--- src/components/djs.js
+++ src/components/djs.js
@@ -3,7 +3,7 @@ import DjCard from './dj-card';
 
 export default ({ data }) => {
   return(
-    <article className="w-100 bg-light-gray " >
+    <article className="w-100 bg-light-gray pv2" >
       <div className="cf pa2">
         <h2 className="center black-70 ttu tracked f1 tc fw4">DJs</h2>
         <div className="center" >
