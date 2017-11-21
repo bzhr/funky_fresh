e063cb49cfc49bdde3164e52a28143cc4634eac3
Bozhdidar <bozidar.hristov@gmail.com>
2017-11-21 15:31:25 +0100

    Padding in about text and better sizing on boxes in footer

--- src/components/about-page.js
+++ src/components/about-page.js
@@ -6,7 +6,7 @@ export default ({ data }) => {
       <section style={orangeBackgroung} className="black-70 baskerville w-100 pa3 pa5-ns bt b--black-1">
         <div className="mw8 center">
           <h2 className="ttu tracked f1 tc fw4">{data.frontmatter.title}</h2>
-          <div className="tc measure mw6 center f3" dangerouslySetInnerHTML={{ __html: data.excerpt }} />
+          <div className="pa3 tc measure mw6 center f3" dangerouslySetInnerHTML={{ __html: data.excerpt }} />
         </div>
       </section>
   );
