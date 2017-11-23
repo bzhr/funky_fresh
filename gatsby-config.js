197dce12cee8ecc23cbeea25446b380570ce82d9
Bozhdidar <bozidar.hristov@gmail.com>
2017-11-23 21:41:21 +0100

    Trying to make imgs query work

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,9 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-image',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
