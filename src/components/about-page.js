b5bf0992810732f63dc8bd7b6c754f2819e79f3f
Bozhdidar <bozidar.hristov@gmail.com>
2017-11-24 20:04:33 +0100

    White instead of orange

--- src/components/about-page.js
+++ src/components/about-page.js
@@ -2,9 +2,8 @@ import React from 'react';
 import logo from '../img/funkyfreshdj.png'
 
 export default ({ data }) => {
-  const orangeBackgroung = {backgroundColor: "#F48120"}
   return (
-      <section style={orangeBackgroung} className="black-70 baskerville pa3 pa5-ns bt b--black-1">
+      <section className="bg-white black-70 baskerville pa3 pa5-ns bt b--black-1">
         <div className="mw8 center" >
           <div className="pt5 pb4 mw5 center">
             <img className="w-100" src={logo} alt="Funky Fresh Logo" />
