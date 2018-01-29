ed06b1878e8afcc1040fd34c759af8b1a67d7070
Bozhdidar <bozidar.hristov@gmail.com>
2018-01-29 16:40:35 +0100

    Logo and about text align vertically on small screens

--- src/components/PromoVideo.js
+++ src/components/PromoVideo.js
@@ -3,27 +3,27 @@ import promoSlika from "../img/promo-slika.jpg";
 
 const videoUrl = require("../img/promo-video.mp4");
 const videoStyle = {
-	display: "block",
-	backgroundSize: "100% 100%"
+  display: "block",
+  backgroundSize: "100% 100%"
 };
 
 export default ({ data }) => {
-	return (
-		<div
-			className="vh-100 bg-near-black"
-		>
-			<video
-				poster={promoSlika}
-				style={videoStyle}
-				className="h-100 w-100 lh-0"
-				id="bgvid"
-				playsInline
-				autoPlay
-				muted
-				loop
-			>
-				<source src={videoUrl} type="video/mp4" />
-			</video>
-		</div>
-	);
+  return (
+    <div
+      className="debug vh-100 bg-near-black"
+    >
+      <video
+        poster={promoSlika}
+        style={videoStyle}
+        className="mt0 h-100 w-100 lh-0"
+        id="bgvid"
+        playsInline
+        autoPlay
+        muted
+        loop
+      >
+        <source src={videoUrl} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
--- src/components/main-page.js
+++ src/components/main-page.js
@@ -12,22 +12,25 @@ export default ({ data }) => {
         className="bg-near-black black-70 pa3 pa5-ns bt b--black-1 w-100"
       >
         <div className="center fl w-100 vh-100 dt">
-          <article className="fl w-50 vh-100 dt">
+          <article className="fl w-50-ns w-100 vh-100 dt">
             <div className="
-            dtc v-mid tc white ph3 ph4-l">
+            dtc v-mid tc white">
               <img
-                className="svg center mw5"
+                className="svg w5 center mw5"
                 style={{ svg: "white" }}
                 src={logo}
                 alt="Funky Fresh Logo"
               />
             </div>
           </article>
-          <div className="fl w-50 vh-100 dt">
-            <div
-              className="dtc v-mid tc white ph3 ph4-l lh-copy pa3 tc measure white f3"
-              dangerouslySetInnerHTML={{ __html: data.excerpt }}
-            />
+          <div className="fl w-50-ns w-100 vh-100 dt">
+            <div className="
+              dtc v-mid tc white">
+              <div
+                className="w-100 center lh-copy pa3 tc measure white f3"
+                dangerouslySetInnerHTML={{ __html: data.excerpt }}
+              />
+            </div>
           </div>
         </div>
       </section>
