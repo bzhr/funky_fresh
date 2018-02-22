97d63a0883232ddaf167466681b4d28d3878ab88
Bozhdidar <bozidar.hristov@gmail.com>
2018-02-22 17:30:57 +0100

    Reversing typography resolver because not cause of error

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,11 +38,11 @@ module.exports = {
         ]
       }
     },
-    // {
-    //   resolve: `gatsby-plugin-typography`,
-    //   options: {
-    //     pathToConfigModule: `src/utils/typography.js`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
   ]
 };
