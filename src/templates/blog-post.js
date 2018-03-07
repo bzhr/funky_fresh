6f6136d5ae422115ae7706bf0d7e24e12ceb5538
Bozhdidar <bozidar.hristov@gmail.com>
2018-03-07 20:45:36 +0100

    Remove code to work

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,8 +5,6 @@ import Img from "gatsby-image";
 
 export default function Template({ data }) {
   const { markdownRemark: post } = data;
-  const img1 = post.fields.imagePath;
-  const img2 = post.fields.imagePath2;
   return (
     <section className="bg-light-gray pa5-ns bt w-100 pa3">
       <Helmet title={`Blog | ${post.frontmatter.title}`} />
@@ -14,16 +12,10 @@ export default function Template({ data }) {
         <h1 className="f3 f1-m f-headline-l ttu tracked f1 fw3 pt5">
           {post.frontmatter.title}
         </h1>
-        {img1 ? (
-          <Img className="ma3-ns ma2" sizes={img1.childImageSharp.sizes} alt="Photo" />
-        ) : null}
         <div
           className="measure lh-copy"
           dangerouslySetInnerHTML={{ __html: post.html }}
         />
-        {img2 ? (
-          <Img className="ma3-ns ma2" sizes={img2.childImageSharp.sizes} alt="Photo" />
-        ) : null}
         <SocialLinks
           postPath={post.frontmatter.path}
           postNode={data.markdownRemark}
@@ -43,6 +35,7 @@ export const singleBlogQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         title
         image
+        image2
       }
     }
   }
