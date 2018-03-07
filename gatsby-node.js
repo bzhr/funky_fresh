c9a196ac795e9297e0dd784cf21d9109bbd45754
Bozhdidar <bozidar.hristov@gmail.com>
2018-03-07 18:22:49 +0100

    Someting is broken

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,14 @@
-const path = require('path');
+const path = require("path");
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators;
 
   return graphql(`
     {
-      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
         edges {
           node {
             excerpt(pruneLength: 400)
@@ -16,6 +19,9 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
               path
               date
               title
+              image
+              image2
+              img
             }
           }
         }
@@ -25,15 +31,31 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
     if (result.errors) {
       return Promise.reject(result.errors);
     }
+    console.log("RESULT", result)
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      let tKey = node.frontmatter.templateKey
-      if (tKey == 'dj-profile' || tKey == 'blog-post') {
+      let tKey = node.frontmatter.templateKey;
+      if (tKey == "dj-profile" || tKey == "blog-post") {
         createPage({
           path: node.frontmatter.path,
-          component: path.resolve(`src/templates/${String(node.frontmatter.templateKey)}.js`),
+          component: path.resolve(
+            `src/templates/${String(node.frontmatter.templateKey)}.js`
+          ),
           context: {} // additional data can be passed via context
         });
-      };
+      }
     });
   });
 };
+
+// exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+//   const { createNodeField } = boundActionCreators
+//   if (node.internal.type === 'MarkdownRemark') {
+//     if (node.frontmatter.templateKey === 'blog-post') {
+//       createNodeField({
+//         node,
+//         name: `happiness`,
+//         value: `is sweet graphql queries`
+//       });
+//     }
+//   }
+// }
