8137a2825abf2c92029905d8d8ec3669895c3e79
Bozhdidar <bozidar.hristov@gmail.com>
2018-05-02 17:30:25 +0200

    Configured Netlify CMS from plugin

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,11 @@
 import CMS from 'netlify-cms'
+import "netlify-cms/dist/cms.css";
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+// import AboutPagePreview from './preview-templates/AboutPagePreview'
+// import BlogPostPreview from './preview-templates/BlogPostPreview'
+// import ProductPagePreview from './preview-templates/ProductPagePreview'
 
-CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+// CMS.registerPreviewStyle('/styles.css')
+// CMS.registerPreviewTemplate('about', AboutPagePreview)
+// CMS.registerPreviewTemplate('products', ProductPagePreview)
+// CMS.registerPreviewTemplate('blog', BlogPostPreview)
