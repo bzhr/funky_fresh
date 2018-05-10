b3617c4d10a04d778a03ce0c52817df53da8d3d4
Bozhdidar <bozidar.hristov@gmail.com>
2018-05-10 19:41:38 +0200

    Set meta tag for FB

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -11,7 +11,12 @@ const Nav = () => (
 
 const TemplateWrapper = ({ children }) => (
   <div className="">
-    <Helmet title="Funky Fresh" />
+    <Helmet title="Funky Fresh">
+    <meta property="og:title" content="Funky Fresh DJs"/>
+    <meta property="og:type" content="music"/>
+    <meta property="og:url" content="http://www.funkyfreshdjs.com"/>
+    <meta property="og:image" content="/media/funkyfreshdj.png" />
+    </Helmet>
     <NavBar />
     {children()}
   </div>
