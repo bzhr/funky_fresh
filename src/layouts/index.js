eb96b0e49c230b870fcc0a83be05cc958c53578b
Bozhdidar <bozidar.hristov@gmail.com>
2018-05-11 00:51:42 +0200

    Add SEO meta tags from SEO component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 import NavBar from './navbar';
+import SEO from '../components/SEO'
 require('tachyons');
 
 const Nav = () => (
@@ -12,14 +13,7 @@ const Nav = () => (
 const TemplateWrapper = ({ children }) => (
   <div className="">
     <Helmet title="Funky Fresh">
-    {/* OpenGraph */}
-    <meta property="og:locale" content="en-US" />
-    <meta property="og:site_name" content="Funky Fresh DJs" />
-    <meta property="og:url" content="https://www.funkyfreshdjs.com" />
-    <meta property="og:type" content="main" />
-    <meta property="og:title" content="Funky Fresh DJs" />
-    <meta property="og:description" content="Funky Frsh DJs info, news and booking" />
-    <meta property="og:image" content="https://www.funkyfreshdjs.com/media/funkyfreshdj.png" />
+      <SEO />
     </Helmet>
     <NavBar />
     {children()}
---
+++ src/utils/website.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // Meta Daten
+  siteTitle: 'Funky Fresh DJs',
+  siteTitleAlt: 'Funky Fresh Hip Hop and Funk DJ collective',
+  siteShortName: 'Funky Fresh',
+  siteLogo: '/media/funkyfreshdj.png',
+  siteBanner: '/social/funkyfreshdj.png',
+  siteUrl: 'https://www.funkyfreshdjs.com',
+  pathPrefix: '/',
+  siteDescription:
+    'Hi! We are a Hip Hop and Funk DJ collective from Macedonia',
+  facebook: 'FunkyFreshSK',
+  // Manifest
+  themeColor: '#3498DB',
+  backgroundColor: '#3498DB',
+};
